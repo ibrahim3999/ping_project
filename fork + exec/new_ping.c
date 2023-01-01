7450f6656defddbf53b96ffbc1165610061df546
Tairsal <73761096+Tairsal@users.noreply.github.com>
2023-01-01 14:05:44 +0200

    Add files via upload

--- fork + exec/new_ping.c
+++ fork + exec/new_ping.c
@@ -1,11 +1,75 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <strings.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+
+#define SERVER_PORT 3000
+#define SERVER_IP_ADDRESS "127.0.0.1"
 
 // run 2 programs using fork + exec
 // command: make clean && make all && ./partb
+int setSock();
+
 int main()
 {
+int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(client_socket == -1) {
+        fprintf(stderr, "Socket ERROR - could not create the socket : %s\n", strerror(errno));
+        exit(1); 
+    }
+    else printf("[+]TCP protocol socket created.\n");
+
+
+    //Sets the first count bytes of destination
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(SERVER_PORT);
+    int rval = inet_pton(AF_INET, (const char*)SERVER_IP_ADDRESS, &server_address.sin_addr);
+    if(rval <= 0) {
+        printf("Failed to convert an IP to Binary format!");
+        exit(1); 
+       
+    }
+
+    // Connect to the receiver
+    int connection = connect(client_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+    if(connection == -1) {
+        fprintf(stderr, "The connecting to watchdog failed - error msg code:%s\n", strerror(errno));
+        exit(1); 
+    }
+    else {
+        printf("[+]ping connected to the watchdog successfully!!\n");
+    }
+
+    //sending the status of the ping to watchdog
+    printf("[+]send the status of the ping\n");
+
+    
+    size = send(client_socket, buffer, sizeof(buffer), 0);
+        if(size<0){
+            printf("ERROR - Send the half 1 of f2.txt-> failed with error!\n");
+            exit(1);
+          }
+
+
+    //get a msg of time out from watchdog if 10 seconds left!
+    recv(client_socket, &xorcheck, sizeof(xorcheck), 0);
+
+
+    //################################################
     char *args[2];
     // compiled watchdog.c by makefile
     args[0] = "./watchdog";
@@ -22,10 +86,9 @@ int main()
     }
     wait(&status); // waiting for child to finish before exiting
     printf("child exit status is: %d", status);
+
+    //close the TCP socket with watchdog
+    close(client_socket);
     return 0;
-}
-void func()
-{
-    printf("Hello\n");
 }
 
--- fork + exec/watchdog.c
+++ fork + exec/watchdog.c
@@ -1,14 +1,96 @@
-#include <stdio.h>
-#include <stdio.h>
-#include <signal.h>
-#include <sys/time.h>
 
-#define TimeOut "timeout"
+#include <stdio.h>
+#include <signal.h>//not important
+#include <sys/time.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+
+#define SERVER_PORT 3000
+#define SERVER_IP_ADDRESS "127.0.0.1"
+
+int setSock();
+
 int main()
 {
+
+    socklen_t length;
+    struct sockaddr_in client_addr;
+    struct sockaddr_in server_addr;
+    socklen_t client_addr_length = sizeof(client_addr);
+    signal(SIGPIPE, SIG_IGN);
+    int receiver_socket = -1;
+
+int setSock(){
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(SERVER_PORT); 
+
+    // create a socket lisener for sender.
+    int receiver_socket = -1;
+    if((receiver_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("Couldn't create a socket listener : %d\n",errno);
+        exit(1);
+        
+    }
+    
+
+    // connect the client
+    
+    if(bind(receiver_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        printf("Bind failed with error code : %d" , errno);
+        exit(1);
+    }
+
+    printf("[+]Receiver connected to the Sender successfully!\n\n");
+
+
+    if(listen(receiver_socket, 500) == -1) {
+        printf("listen failed with error code : %d",errno);
+        exit(1);
+    }
+    return receiver_socket;
+}
+
+receiver_socket = setSock(); //Creating the socket
+    
+    printf("[+]TCP protocol socket created -> Waiting for connection with sender\n");
+
+    int countbytes = 0;
+
+        memset(&client_addr, 0, sizeof(client_addr));
+        client_addr_length = sizeof(client_addr);
+
+        //The receiver accept the connection
+        
+        int client_socket = accept(receiver_socket, (struct sockaddr *) &client_addr, &client_addr_length);
+        if (client_socket == -1) {
+            printf("listen failed with error code :%d \n",errno);
+            exit(1);
+        } else {
+            printf("[+]Sender number connection accepted\n");
+        }
+    /////
+
+    printf("\nhello partb\n");
     char buffreExit[1];
     char buffer_timeout[8];
-    printf("hello partb");
+    strcpy(buffer_timeout,"timeout");
+    
+    
     /*
     while (timer < 10seconds)
     {
@@ -39,7 +121,8 @@ int main()
         }
         else break;
     }
-    send(client_socket,TimeOut, sizeof(buffer_timeout),0);
+    send(client_socket,buffer_timeout, sizeof(buffer_timeout),0);
 
+    close(receiver_socket);
         return 0;
 }
